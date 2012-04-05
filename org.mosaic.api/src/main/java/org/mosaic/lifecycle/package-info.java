f6cfe2a4cb2694fd015ba4be978525714347693b
Arik Kfir <arik.kfir@gmail.com>
2012-04-05 21:49:43 +0300

    Added TODOs

--- org.mosaic.api/src/main/java/org/mosaic/lifecycle/package-info.java
+++ org.mosaic.api/src/main/java/org/mosaic/lifecycle/package-info.java
@@ -1,3 +1,5 @@
 @Version( "1.0.0" ) package org.mosaic.lifecycle;
 
 import aQute.bnd.annotation.Version;
+
+//TODO 4/4/12: create BundleState class which describes, in addition to the OSGi state, the bundle publication state
