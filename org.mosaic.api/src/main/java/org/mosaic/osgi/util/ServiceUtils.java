18cc04c2beb17b2aea8b2df8cc9aad2dd242bc5e
Arik Kfir <arik.kfir@gmail.com>
2012-04-07 02:21:15 +0300

    Add ServiceUtils class

---
+++ org.mosaic.api/src/main/java/org/mosaic/osgi/util/ServiceUtils.java
@@ -0,0 +1,20 @@
+package org.mosaic.osgi.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * @author arik
+ */
+public abstract class ServiceUtils {
+
+    public static Map<String, Object> getServiceProperties( ServiceReference<?> serviceReference ) {
+        Map<String, Object> properties = new HashMap<>();
+        for( String property : serviceReference.getPropertyKeys() ) {
+            properties.put( property, serviceReference.getProperty( property ) );
+        }
+        return properties;
+    }
+
+}
