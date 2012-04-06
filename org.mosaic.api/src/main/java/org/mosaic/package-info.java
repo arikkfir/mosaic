074a94befe97931ce063386a7de760c29a1a97cd
Arik Kfir <arik.kfir@gmail.com>
2012-04-06 03:41:13 +0300

    Remove duplicate TODO

--- org.mosaic.api/src/main/java/org/mosaic/package-info.java
+++ org.mosaic.api/src/main/java/org/mosaic/package-info.java
@@ -1,5 +1,3 @@
 @Version( "1.0.0" ) package org.mosaic;
 
 import aQute.bnd.annotation.Version;
-
-//TODO 4/4/12: create BundleState class which describes, in addition to the OSGi state, the bundle publication state
