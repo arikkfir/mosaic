575a285f65f0f1cb670c91d9831f7205f009b1ad
Arik Kfir <arik.kfir@gmail.com>
2012-04-10 15:47:52 +0300

    Working draft of configuration manager bundle

--- org.mosaic.server/org.mosaic.server.config/src/main/java/org/mosaic/server/config/impl/ConfigurationImpl.java
+++ org.mosaic.server/org.mosaic.server.config/src/main/java/org/mosaic/server/config/impl/ConfigurationImpl.java
@@ -1,18 +1,102 @@
 package org.mosaic.server.config.impl;
 
-import java.util.HashMap;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArraySet;
 import org.mosaic.config.Configuration;
+import org.mosaic.lifecycle.MethodEndpointInfo;
+import org.mosaic.logging.Logger;
+import org.mosaic.logging.LoggerFactory;
 import org.springframework.core.convert.ConversionService;
+
+import static java.nio.file.Files.*;
+import static java.nio.file.StandardOpenOption.READ;
 
 /**
  * @author arik
  */
-public class ConfigurationImpl extends HashMap<String, String> implements Configuration {
+public class ConfigurationImpl implements Configuration {
+
+    private static final Logger LOG = LoggerFactory.getBundleLogger( ConfigurationImpl.class );
+
+    private final String name;
+
+    private final Path path;
 
     private final ConversionService conversionService;
 
-    public ConfigurationImpl( ConversionService conversionService ) {
+    private final Set<MethodEndpointInfo> listeners = new CopyOnWriteArraySet<>();
+
+    private Map<String, String> data = Collections.emptyMap();
+
+    private long modificationTime;
+
+    public ConfigurationImpl( Path path, ConversionService conversionService ) {
+        this.path = path;
+        String fileName = this.path.getFileName().toString();
+        this.name = fileName.substring( 0, fileName.indexOf( '.' ) );
         this.conversionService = conversionService;
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public synchronized void addListener( MethodEndpointInfo listener ) {
+        this.listeners.add( listener );
+        refresh();
+        invokeListener( listener );
+    }
+
+    public synchronized Collection<MethodEndpointInfo> getListeners() {
+        return this.listeners;
+    }
+
+    public synchronized void removeListener( MethodEndpointInfo listener ) {
+        this.listeners.remove( listener );
+    }
+
+    public synchronized void refresh() {
+        if( isDirectory( this.path ) || !exists( this.path ) || !isReadable( this.path ) ) {
+
+            if( this.modificationTime > 0 ) {
+
+                LOG.warn( "Configuration '{}' no longer exists/readable at: {}", this.name, this.path );
+                this.data = Collections.emptyMap();
+                this.modificationTime = 0;
+                invokeListeners();
+
+            }
+
+        } else {
+
+            try {
+                long modificationTime = getLastModifiedTime( this.path ).toMillis();
+                if( modificationTime > this.modificationTime ) {
+                    this.modificationTime = modificationTime;
+
+                    LOG.info( "Refreshing configuration '{}' from: {}", this.name, this.path );
+                    Map<String, String> data = new HashMap<>();
+                    try( InputStream inputStream = newInputStream( this.path, READ ) ) {
+
+                        Properties properties = new Properties();
+                        properties.load( inputStream );
+                        for( String propertyName : properties.stringPropertyNames() ) {
+                            data.put( propertyName, properties.getProperty( propertyName ) );
+                        }
+
+                    }
+
+                    this.data = data;
+                    invokeListeners();
+                }
+            } catch( IOException e ) {
+                LOG.error( "Could not refresh configuration '{}': {}", this.path.getFileName().toString(), e.getMessage(), e );
+            }
+
+        }
     }
 
     public <T> T getAs( String key, Class<T> type ) {
@@ -26,6 +110,82 @@ public class ConfigurationImpl extends HashMap<String, String> implements Config
             return defaultValue;
         } else {
             return this.conversionService.convert( stringValue, type );
+        }
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey( Object key ) {
+        return data.containsKey( key );
+    }
+
+    @Override
+    public boolean containsValue( Object value ) {
+        return data.containsValue( value );
+    }
+
+    @Override
+    public String get( Object key ) {
+        return data.get( key );
+    }
+
+    @Override
+    public String put( String key, String value ) {
+        return data.put( key, value );
+    }
+
+    @Override
+    public String remove( Object key ) {
+        return data.remove( key );
+    }
+
+    @Override
+    public void putAll( Map<? extends String, ? extends String> m ) {
+        data.putAll( m );
+    }
+
+    @Override
+    public void clear() {
+        data.clear();
+    }
+
+    @Override
+    public Set<String> keySet() {
+        return data.keySet();
+    }
+
+    @Override
+    public Collection<String> values() {
+        return data.values();
+    }
+
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        return data.entrySet();
+    }
+
+    private void invokeListeners() {
+        for( MethodEndpointInfo listener : this.listeners ) {
+            invokeListener( listener );
+        }
+    }
+
+    private void invokeListener( MethodEndpointInfo listener ) {
+        try {
+            LOG.debug( "Invoking @ConfigListener '{}' for configuration '{}'", listener, this.name );
+            listener.invoke( this );
+        } catch( Exception e ) {
+            LOG.error( "Error invoking @ConfigListener for configuration '{}': {}",
+                       this.path.getFileName().toString(), e.getMessage(), e );
         }
     }
 }
--- org.mosaic.server/org.mosaic.server.config/src/main/java/org/mosaic/server/config/impl/ConfigurationManager.java
+++ org.mosaic.server/org.mosaic.server.config/src/main/java/org/mosaic/server/config/impl/ConfigurationManager.java
@@ -1,8 +1,13 @@
 package org.mosaic.server.config.impl;
 
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.mosaic.MosaicHome;
 import org.mosaic.config.ConfigListener;
 import org.mosaic.lifecycle.MethodEndpointInfo;
@@ -11,7 +16,13 @@ import org.mosaic.lifecycle.ServiceRef;
 import org.mosaic.lifecycle.ServiceUnbind;
 import org.mosaic.logging.Logger;
 import org.mosaic.logging.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Component;
+
+import static java.nio.file.Files.exists;
+import static java.nio.file.Files.newDirectoryStream;
+import static org.springframework.core.annotation.AnnotationUtils.getValue;
 
 /**
  * @author arik
@@ -21,9 +32,20 @@ public class ConfigurationManager {
 
     private static final Logger LOG = LoggerFactory.getBundleLogger( ConfigurationManager.class );
 
+    private static final long SCAN_INTERVAL = 1000;
+
+    private final Map<String, ConfigurationImpl> configurations = new ConcurrentHashMap<>();
+
+    private ConversionService conversionService;
+
     private MosaicHome mosaicHome;
 
-    private Set<MethodEndpointInfo> listeners = new CopyOnWriteArraySet<>();
+    private ConfigurationManager.Scanner scanner;
+
+    @Autowired
+    public void setConversionService( ConversionService conversionService ) {
+        this.conversionService = conversionService;
+    }
 
     @ServiceRef
     public void setMosaicHome( MosaicHome mosaicHome ) {
@@ -32,22 +54,85 @@ public class ConfigurationManager {
 
     @ServiceBind
     public void addListener( MethodEndpointInfo methodEndpointInfo ) {
-        if( methodEndpointInfo.getType().equals( ConfigListener.class ) ) {
-            LOG.info( "Added @ConfigListener '{}'", methodEndpointInfo );
-            this.listeners.add( methodEndpointInfo );
+        if( methodEndpointInfo.isOfType( ConfigListener.class ) ) {
+            String configurationName = getValue( methodEndpointInfo.getType() ).toString();
+            getConfiguration( configurationName ).addListener( methodEndpointInfo );
         }
     }
 
     @ServiceUnbind
     public void removeListener( MethodEndpointInfo methodEndpointInfo ) {
-        if( methodEndpointInfo.getType().equals( ConfigListener.class ) ) {
-            this.listeners.remove( methodEndpointInfo );
-            LOG.info( "Removed @ConfigListener '{}'", methodEndpointInfo );
+        if( methodEndpointInfo.isOfType( ConfigListener.class ) ) {
+            String configurationName = getValue( methodEndpointInfo.getType() ).toString();
+            getConfiguration( configurationName ).removeListener( methodEndpointInfo );
         }
     }
 
     @PostConstruct
     public void init() {
-        LOG.info( "Started configuration manager" );
+        this.scanner = new Scanner();
+        Thread t = new Thread( scanner, "ConfigurationsWatcher" );
+        t.setDaemon( true );
+        t.start();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        if( this.scanner != null ) {
+            this.scanner.stop = true;
+        }
+        LOG.info( "Stopped configuration manager" );
+    }
+
+    private synchronized void scan() {
+        try( DirectoryStream<Path> stream = newDirectoryStream( this.mosaicHome.getEtc(), "*.properties" ) ) {
+            for( Path path : stream ) {
+                path = path.normalize().toAbsolutePath();
+                getConfiguration( path.getFileName().toString() ).refresh();
+            }
+
+            Iterator<Map.Entry<String, ConfigurationImpl>> iterator = this.configurations.entrySet().iterator();
+            while( iterator.hasNext() ) {
+                Map.Entry<String, ConfigurationImpl> entry = iterator.next();
+                ConfigurationImpl configuration = entry.getValue();
+                if( !exists( configuration.getPath() ) && configuration.getListeners().isEmpty() ) {
+                    iterator.remove();
+                }
+            }
+
+        } catch( IOException e ) {
+            LOG.error( "Could not watch directory '{}': {}", this.mosaicHome.getEtc(), e.getMessage(), e );
+        }
+    }
+
+    private synchronized ConfigurationImpl getConfiguration( String configName ) {
+        if( configName.toLowerCase().endsWith( ".properties" ) ) {
+            configName = configName.substring( 0, configName.length() - ".properties".length() );
+        }
+
+        ConfigurationImpl configuration = this.configurations.get( configName );
+        if( configuration == null ) {
+            Path file = this.mosaicHome.getEtc().resolve( configName + ".properties" );
+            configuration = new ConfigurationImpl( file, this.conversionService );
+            this.configurations.put( configName, configuration );
+        }
+        return configuration;
+    }
+
+    private class Scanner implements Runnable {
+
+        private boolean stop;
+
+        @Override
+        public void run() {
+            while( !this.stop ) {
+                try {
+                    Thread.sleep( SCAN_INTERVAL );
+                } catch( InterruptedException e ) {
+                    break;
+                }
+                scan();
+            }
+        }
     }
 }
