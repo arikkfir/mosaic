78a791ca9875c8d50f88691aace9c6a094949277
Arik Kfir <arik.kfir@gmail.com>
2012-04-10 23:56:31 +0300

    Add 'shell' package in server API for defining new shell commands

---
+++ org.mosaic.server/org.mosaic.server.api/src/main/java/org/mosaic/server/shell/ShellCommand.java
@@ -0,0 +1,17 @@
+package org.mosaic.server.shell;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.mosaic.lifecycle.MethodEndpointMarker;
+
+/**
+ * @author arik
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Target( ElementType.METHOD )
+@MethodEndpointMarker
+public @interface ShellCommand {
+
+}
