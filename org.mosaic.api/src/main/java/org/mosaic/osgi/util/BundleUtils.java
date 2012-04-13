429f6abb3554ccb4a1935c925f2bc047b8ae4553
Arik Kfir <arik.kfir@gmail.com>
2012-04-14 01:40:42 +0300

    Added some methods

--- org.mosaic.api/src/main/java/org/mosaic/osgi/util/BundleUtils.java
+++ org.mosaic.api/src/main/java/org/mosaic/osgi/util/BundleUtils.java
@@ -1,11 +1,9 @@
 package org.mosaic.osgi.util;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.*;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 
 /**
  * @author arik
@@ -19,6 +17,59 @@ public abstract class BundleUtils {
         } else {
             return Arrays.asList( bundles );
         }
+    }
+
+    public static Collection<Bundle> findBundlesInStates( BundleContext bundleContext, Integer... states ) {
+        Collection<Integer> bundleStates = Arrays.asList( states );
+        Collection<Bundle> resolvedBundles = new LinkedList<>();
+        for( Bundle bundle : getAllBundles( bundleContext ) ) {
+            if( bundleStates.contains( bundle.getState() ) ) {
+                resolvedBundles.add( bundle );
+            }
+        }
+        return resolvedBundles;
+    }
+
+    public static List<Bundle> findMatchingBundles( BundleContext bundleContext, boolean exact, String... filters ) {
+        List<Bundle> matches = new LinkedList<>();
+        for( Bundle bundle : getAllBundles( bundleContext ) ) {
+
+            boolean match = true;
+            if( filters != null && filters.length > 0 ) {
+
+                String bundleName = bundle.getHeaders().get( Constants.BUNDLE_NAME );
+                String symbolicName = bundle.getSymbolicName();
+                match = false;
+                for( String arg : filters ) {
+                    if( exact && ( bundleName.equalsIgnoreCase( arg ) || symbolicName.equalsIgnoreCase( arg ) ) ) {
+                        match = true;
+                        break;
+                    } else if( !exact && ( bundleName.contains( arg ) || symbolicName.contains( arg ) ) ) {
+                        match = true;
+                        break;
+                    }
+                }
+
+            }
+
+            if( match ) {
+                matches.add( bundle );
+            }
+        }
+        return matches;
+    }
+
+    public static List<Bundle> filterBundlesByState( List<Bundle> bundles, Integer... states ) {
+        List<Integer> statesList = Arrays.asList( states );
+        List<Bundle> matches = new LinkedList<>();
+        for( Bundle bundle : bundles ) {
+
+            if( statesList.contains( bundle.getState() ) ) {
+                matches.add( bundle );
+            }
+
+        }
+        return matches;
     }
 
     public static String toString( Bundle bundle ) {
@@ -37,16 +88,5 @@ public abstract class BundleUtils {
             }
         }
         return "";
-    }
-
-    public static Collection<Bundle> findBundlesInStates( BundleContext bundleContext, Integer... states ) {
-        Collection<Integer> bundleStates = Arrays.asList( states );
-        Collection<Bundle> resolvedBundles = new LinkedList<>();
-        for( Bundle bundle : getAllBundles( bundleContext ) ) {
-            if( bundleStates.contains( bundle.getState() ) ) {
-                resolvedBundles.add( bundle );
-            }
-        }
-        return resolvedBundles;
     }
 }
