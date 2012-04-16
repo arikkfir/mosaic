3f3fe7f7fe2a371ac96eb8024e98b14c2cf43c2c
Arik Kfir <arik.kfir@gmail.com>
2012-04-17 01:42:55 +0300

    Add test module (not unit tests - just a test deployment)

---
+++ org.mosaic.test/org.mosaic.test.tx/src/main/java/org/mosaic/test/impl/WeavingTest.java
@@ -0,0 +1,14 @@
+package org.mosaic.test.impl;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author arik
+ */
+@Component
+public class WeavingTest {
+
+    public void test() {
+        System.out.println( "I'm weaved?" );
+    }
+}
