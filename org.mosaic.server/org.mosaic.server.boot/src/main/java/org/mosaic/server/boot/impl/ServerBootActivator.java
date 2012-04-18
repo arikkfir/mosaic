381ecc507169f79a52d96d403d537dbeabac9568
Arik Kfir <arik.kfir@gmail.com>
2012-04-18 21:20:02 +0300

    Embed MosaicHome in ServerBootActivator

--- org.mosaic.server/org.mosaic.server.boot/src/main/java/org/mosaic/server/boot/impl/ServerBootActivator.java
+++ org.mosaic.server/org.mosaic.server.boot/src/main/java/org/mosaic/server/boot/impl/ServerBootActivator.java
@@ -1,5 +1,7 @@
 package org.mosaic.server.boot.impl;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.mosaic.MosaicHome;
 import org.mosaic.server.boot.impl.publish.spring.OsgiSpringNamespacePlugin;
 import org.osgi.framework.BundleActivator;
@@ -40,5 +42,59 @@ public class ServerBootActivator implements BundleActivator {
 
         this.logListener.close();
         this.logListener = null;
+    }
+
+    private class MosaicHomeService implements MosaicHome {
+
+        private final Path home;
+
+        private final Path boot;
+
+        private final Path deploy;
+
+        private final Path etc;
+
+        private final Path server;
+
+        private final Path work;
+
+        private MosaicHomeService() {
+            this.home = Paths.get( System.getProperty( "mosaic.home" ) );
+            this.boot = Paths.get( System.getProperty( "mosaic.home.boot" ) );
+            this.deploy = Paths.get( System.getProperty( "mosaic.home.deploy" ) );
+            this.etc = Paths.get( System.getProperty( "mosaic.home.etc" ) );
+            this.server = Paths.get( System.getProperty( "mosaic.home.server" ) );
+            this.work = Paths.get( System.getProperty( "mosaic.home.work" ) );
+        }
+
+        @Override
+        public Path getHome() {
+            return this.home;
+        }
+
+        @Override
+        public Path getBoot() {
+            return this.boot;
+        }
+
+        @Override
+        public Path getDeploy() {
+            return this.deploy;
+        }
+
+        @Override
+        public Path getEtc() {
+            return this.etc;
+        }
+
+        @Override
+        public Path getServer() {
+            return this.server;
+        }
+
+        @Override
+        public Path getWork() {
+            return this.work;
+        }
     }
 }
