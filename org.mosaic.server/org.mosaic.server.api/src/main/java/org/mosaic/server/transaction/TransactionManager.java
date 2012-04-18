c12f0304f3380f73a09d69e329c6a53eefb6b8f9
Arik Kfir <arik.kfir@gmail.com>
2012-04-18 19:47:30 +0300

    Add TODO

--- org.mosaic.server/org.mosaic.server.api/src/main/java/org/mosaic/server/transaction/TransactionManager.java
+++ org.mosaic.server/org.mosaic.server.api/src/main/java/org/mosaic/server/transaction/TransactionManager.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * @author arik
+ * @todo we should avoid exposing PlatformTransactionManager here and provide our own API (its implementation should redirect to the Spring API)
  */
 public interface TransactionManager extends DataSource, PlatformTransactionManager {
 
