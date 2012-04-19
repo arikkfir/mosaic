2402aa9adc885fe80478317bc42f0bd494dc2a1e
Arik Kfir <arik.kfir@gmail.com>
2012-04-19 23:58:38 +0300

    add web.application module placeholder

---
+++ org.mosaic.server/org.mosaic.server.web/org.mosaic.server.web.application/src/main/java/org/mosaic/server/web/application/impl/HttpApplicationManagerImpl.java
@@ -0,0 +1,12 @@
+package org.mosaic.server.web.application.impl;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author arik
+ */
+@Component
+public class HttpApplicationManagerImpl {
+
+
+}
