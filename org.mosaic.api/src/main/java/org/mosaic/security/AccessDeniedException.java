37d4e17fb98957a5299f368eed25bdbee830b9be
Arik Kfir <arik.kfir@gmail.com>
2012-05-01 03:31:51 +0300

    add AccessDeniedException and missing package-info

---
+++ org.mosaic.api/src/main/java/org/mosaic/security/AccessDeniedException.java
@@ -0,0 +1,12 @@
+package org.mosaic.security;
+
+/**
+ * @author arik
+ */
+public class AccessDeniedException extends Exception {
+
+    public AccessDeniedException() {
+        super( "Access denied" );
+    }
+
+}
