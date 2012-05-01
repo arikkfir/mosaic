c0d71ecdd4c6790888e76349ba350bc41e7b4e6f
Arik Kfir <arik.kfir@gmail.com>
2012-05-01 19:35:42 +0300

    add handler annotations API

---
+++ org.mosaic.api/src/main/java/org/mosaic/web/handler/annotation/Service.java
@@ -0,0 +1,19 @@
+package org.mosaic.web.handler.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.mosaic.lifecycle.MethodEndpointMarker;
+
+/**
+ * @author arik
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Target( ElementType.METHOD )
+@MethodEndpointMarker
+public @interface Service {
+
+    String[] value();
+
+}
