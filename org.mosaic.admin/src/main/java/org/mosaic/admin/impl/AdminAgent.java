61413dfee2f816fef61b92800a6dd4fce18d9435
arikkfir <arik.kfir@gmail.com>
2012-05-02 22:16:38 +0300

    Further implementation

--- org.mosaic.admin/src/main/java/org/mosaic/admin/impl/AdminAgent.java
+++ org.mosaic.admin/src/main/java/org/mosaic/admin/impl/AdminAgent.java
@@ -4,8 +4,10 @@ import org.mosaic.util.logging.Logger;
 import org.mosaic.util.logging.LoggerFactory;
 import org.mosaic.util.logging.Trace;
 import org.mosaic.web.HttpRequest;
+import org.mosaic.web.handler.InterceptorChain;
 import org.mosaic.web.handler.annotation.Controller;
 import org.mosaic.web.handler.annotation.Get;
+import org.mosaic.web.handler.annotation.Interceptor;
 import org.mosaic.web.handler.annotation.Service;
 import org.springframework.stereotype.Component;
 
@@ -30,6 +32,14 @@ public class AdminAgent
     @Trace
     public void myController( HttpRequest request )
     {
+        System.out.println( "Lala! Request=" + request );
     }
 
+    @Get
+    @Interceptor( "/controllers/arik" )
+    public Object interceptMe( HttpRequest request, InterceptorChain chain ) throws Exception
+    {
+        System.out.println( "Intercepting..." );
+        return chain.next( );
+    }
 }
