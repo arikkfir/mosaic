640916f8783e6b71cf1eedd0679f364724459f43
arikkfir <arik.kfir@gmail.com>
2012-05-02 21:01:49 +0300

    Add missing @MethodEndpointMarker annotation (and reformat)

--- org.mosaic.api/src/main/java/org/mosaic/web/handler/annotation/ExceptionHandler.java
+++ org.mosaic.api/src/main/java/org/mosaic/web/handler/annotation/ExceptionHandler.java
@@ -4,13 +4,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.mosaic.lifecycle.MethodEndpointMarker;
 
 /**
  * @author arik
  */
 @Retention( RetentionPolicy.RUNTIME )
 @Target( ElementType.METHOD )
+@MethodEndpointMarker
 public @interface ExceptionHandler
 {
-
 }
