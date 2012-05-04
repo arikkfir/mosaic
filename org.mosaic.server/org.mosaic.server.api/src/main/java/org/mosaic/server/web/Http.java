edc6661ddfd4996050a6d176106d72975ea77a43
arikkfir <arik.kfir@gmail.com>
2012-05-04 13:23:42 +0300

    Add request to MDC

--- org.mosaic.server/org.mosaic.server.api/src/main/java/org/mosaic/server/web/Http.java
+++ org.mosaic.server/org.mosaic.server.api/src/main/java/org/mosaic/server/web/Http.java
@@ -1,5 +1,6 @@
 package org.mosaic.server.web;
 
+import org.mosaic.util.logging.LogContext;
 import org.mosaic.web.HttpRequest;
 
 /**
@@ -17,5 +18,19 @@ public abstract class Http
     public static void setRequest( HttpRequest request )
     {
         REQUEST.set( request );
+        if( request != null )
+        {
+            LogContext.put( "appName", request.getApplication().getName() );
+            LogContext.put( "reqClient", request.getClientAddress() );
+            LogContext.put( "reqMethod", request.getMethod() );
+            LogContext.put( "reqPath", request.getUrl().getPath() );
+        }
+        else
+        {
+            LogContext.remove( "appName" );
+            LogContext.remove( "reqClient" );
+            LogContext.remove( "reqMethod" );
+            LogContext.remove( "reqPath" );
+        }
     }
 }
