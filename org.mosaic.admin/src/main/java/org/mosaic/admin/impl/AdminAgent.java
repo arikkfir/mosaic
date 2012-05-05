08b5a1658a33a0528993cb9608073065f5a3eadb
arikkfir <arik.kfir@gmail.com>
2012-05-05 17:49:25 +0300

    testing

--- org.mosaic.admin/src/main/java/org/mosaic/admin/impl/AdminAgent.java
+++ org.mosaic.admin/src/main/java/org/mosaic/admin/impl/AdminAgent.java
@@ -1,5 +1,7 @@
 package org.mosaic.admin.impl;
 
+import org.mosaic.util.logging.Logger;
+import org.mosaic.util.logging.LoggerFactory;
 import org.mosaic.util.logging.Trace;
 import org.mosaic.web.HttpRequest;
 import org.mosaic.web.handler.InterceptorChain;
@@ -15,26 +17,50 @@ import org.springframework.stereotype.Component;
 @Component
 public class AdminAgent
 {
+    private static final Logger LOG = LoggerFactory.getLogger( AdminAgent.class );
+
+    public static class Person
+    {
+        public String firstName;
+
+        public String lastName;
+
+        public String type;
+
+        public Person()
+        {
+        }
+
+        public Person( String firstName, String lastName, String type )
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.type = type;
+        }
+    }
+
     @Get
     @Service( "/services/arik" )
     @Trace
-    public void myService( HttpRequest request )
+    public Person myService( HttpRequest request )
     {
+        return new Person( "Arik", "Kfir", "Service" );
     }
 
     @Get
     @Controller( "/controllers/arik" )
     @Trace
-    public void myController( HttpRequest request )
+    public Person myController( HttpRequest request )
     {
-        System.out.println( "Lala! Request=" + request );
+        return new Person( "Arik", "Kfir", "Controller" );
     }
 
     @Get
     @Interceptor( "/controllers/arik" )
     public Object interceptMe( HttpRequest request, InterceptorChain chain ) throws Exception
     {
-        System.out.println( "Intercepting..." );
+        Object result = chain.next();
+        LOG.info( "Handler returned: {}", result );
         return chain.next();
     }
 }
