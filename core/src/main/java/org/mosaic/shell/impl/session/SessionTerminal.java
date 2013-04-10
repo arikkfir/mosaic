4073a817f61c5375ea8465fcf3618721f0e0daaf
arikkfir <arik.kfir@gmail.com>
2013-04-11 00:30:47 +0300

    Support setting terminal width/height programmatically

--- core/src/main/java/org/mosaic/shell/impl/session/SessionTerminal.java
+++ core/src/main/java/org/mosaic/shell/impl/session/SessionTerminal.java
@@ -1,5 +1,7 @@
 package org.mosaic.shell.impl.session;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import jline.TerminalSupport;
 import org.apache.sshd.server.Environment;
 
@@ -8,12 +10,29 @@ import org.apache.sshd.server.Environment;
  */
 public class SessionTerminal extends TerminalSupport
 {
+    @Nonnull
     private final Environment environment;
 
-    public SessionTerminal( Environment environment )
+    @Nullable
+    private Integer customWidth;
+
+    @Nullable
+    private Integer customHeight;
+
+    public SessionTerminal( @Nonnull Environment environment )
     {
         super( true );
         this.environment = environment;
+    }
+
+    public SessionTerminal( @Nonnull Environment environment,
+                            @Nullable Integer customWidth,
+                            @Nullable Integer customHeight )
+    {
+        super( true );
+        this.environment = environment;
+        this.customWidth = customWidth;
+        this.customHeight = customHeight;
     }
 
     @Override
@@ -27,6 +46,10 @@ public class SessionTerminal extends TerminalSupport
     @Override
     public int getWidth()
     {
+        if( this.customWidth != null )
+        {
+            return this.customWidth;
+        }
         String value = this.environment.getEnv().get( Environment.ENV_COLUMNS );
         return value == null ? 80 : Integer.valueOf( value );
     }
@@ -34,6 +57,10 @@ public class SessionTerminal extends TerminalSupport
     @Override
     public int getHeight()
     {
+        if( this.customHeight != null )
+        {
+            return this.customHeight;
+        }
         String value = this.environment.getEnv().get( Environment.ENV_LINES );
         return value == null ? 24 : Integer.valueOf( value );
     }
