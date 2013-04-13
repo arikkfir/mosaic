6a6a90e17387fe472653eb1fbb008413338e063f
arikkfir <arik.kfir@gmail.com>
2013-04-14 01:02:11 +0300

    Annotate 'Password.getPassword()' method as @Nullable

--- api/src/main/java/org/mosaic/security/credentials/Password.java
+++ api/src/main/java/org/mosaic/security/credentials/Password.java
@@ -1,12 +1,14 @@
 package org.mosaic.security.credentials;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * @author arik
  */
 public interface Password
 {
+    @Nullable
     char[] getPassword();
 
     boolean check( @Nonnull char[] password );
