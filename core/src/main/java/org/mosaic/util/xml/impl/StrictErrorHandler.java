dd5ad3dad27b440547bf6678cd06cc4c4e5aaa57
arikkfir <arik.kfir@gmail.com>
2013-04-14 02:46:51 +0300

    Add StrictErrorHandler class in org.mosaic.util.xml.impl

---
+++ core/src/main/java/org/mosaic/util/xml/impl/StrictErrorHandler.java
@@ -0,0 +1,31 @@
+package org.mosaic.util.xml.impl;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ * @author arik
+ */
+public class StrictErrorHandler implements ErrorHandler
+{
+    public static final StrictErrorHandler INSTANCE = new StrictErrorHandler();
+
+    @Override
+    public void warning( SAXParseException exception ) throws SAXException
+    {
+        throw exception;
+    }
+
+    @Override
+    public void error( SAXParseException exception ) throws SAXException
+    {
+        throw exception;
+    }
+
+    @Override
+    public void fatalError( SAXParseException exception ) throws SAXException
+    {
+        throw exception;
+    }
+}
