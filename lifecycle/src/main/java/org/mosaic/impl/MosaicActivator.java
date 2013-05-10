7746773750753e06b3cf856f7e74da6b1b0bb7fb
arikkfir <arik.kfir@gmail.com>
2013-05-11 02:09:16 +0300

    The 'lifecycle' module now EMBEDS the jcl-over-slf4j and log4j-over-slf4j and deploys them as bundles on startup This removes the need to have them bundled in the zip distribution or deployed by the launcher

--- lifecycle/src/main/java/org/mosaic/impl/MosaicActivator.java
+++ lifecycle/src/main/java/org/mosaic/impl/MosaicActivator.java
@@ -1,11 +1,22 @@
 package org.mosaic.impl;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Enumeration;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.mosaic.lifecycle.impl.util.BundleUtils;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static java.nio.file.Files.copy;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 /**
  * @author arik
@@ -29,6 +40,9 @@ public class MosaicActivator implements BundleActivator
     {
         MosaicActivator.bundleContext = bundleContext;
 
+        deployEmbeddedBundle( "jcl-over-slf4j" );
+        deployEmbeddedBundle( "log4j-over-slf4j" );
+
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext();
         applicationContext.setConfigLocation( "/lifecycle-beans.xml" );
         applicationContext.setClassLoader( BundleUtils.getClassLoader( bundleContext ) );
@@ -49,5 +63,43 @@ public class MosaicActivator implements BundleActivator
             this.applicationContext = null;
         }
         MosaicActivator.bundleContext = null;
+    }
+
+    private void deployEmbeddedBundle( @Nonnull String name ) throws IOException, BundleException
+    {
+        BundleContext bc = bundleContext;
+        if( bc == null )
+        {
+            throw new IllegalStateException( "Bundle context has not been set" );
+        }
+
+        Path bundleFile = Files.createTempFile( name, ".jar" );
+        try( InputStream in = findEmbeddedBundle( name ).openStream() )
+        {
+            copy( in, bundleFile, REPLACE_EXISTING );
+        }
+
+        bc.installBundle( bundleFile.toUri().toString() );
+    }
+
+    @Nonnull
+    private URL findEmbeddedBundle( @Nonnull String name )
+    {
+        BundleContext bc = bundleContext;
+        if( bc == null )
+        {
+            throw new IllegalStateException( "Bundle context has not been set" );
+        }
+
+        Bundle bundle = bc.getBundle();
+        Enumeration<URL> jcl = bundle.findEntries( "/", name + "-*.jar", false );
+        if( jcl.hasMoreElements() )
+        {
+            return jcl.nextElement();
+        }
+        else
+        {
+            throw new IllegalStateException( "Could not find embedded bundle '" + name + "' in lifecycle bundle" );
+        }
     }
 }
