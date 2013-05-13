874ec374219e77a2774004e87c5fb136c9bab165
arikkfir <arik.kfir@gmail.com>
2013-05-13 23:18:54 +0300

    Add @Nonnull to method parameters

--- core/src/main/java/org/mosaic/shell/impl/command/std/Lifecycle.java
+++ core/src/main/java/org/mosaic/shell/impl/command/std/Lifecycle.java
@@ -1,5 +1,6 @@
 package org.mosaic.shell.impl.command.std;
 
+import javax.annotation.Nonnull;
 import org.mosaic.Server;
 import org.mosaic.lifecycle.annotation.Bean;
 import org.mosaic.lifecycle.annotation.ServiceRef;
@@ -14,18 +15,18 @@ public class Lifecycle
     private Server server;
 
     @ServiceRef
-    public void setServer( Server server )
+    public void setServer( @Nonnull Server server )
     {
         this.server = server;
     }
 
-    @Command(name = "restart-server", label = "Restart Mosaic", desc = "Restarts the Mosaic server.")
+    @Command( name = "restart-server", label = "Restart Mosaic", desc = "Restarts the Mosaic server." )
     public void restartServer()
     {
         server.restart();
     }
 
-    @Command(name = "shutdown-server", label = "Shutdown Mosaic", desc = "Shuts down the Mosaic server.")
+    @Command( name = "shutdown-server", label = "Shutdown Mosaic", desc = "Shuts down the Mosaic server." )
     public void shutdownServer()
     {
         server.shutdown();
