530999beea582ffa95fc1de8d88a42702b15efa0
arikkfir <arik.kfir@gmail.com>
2013-05-26 00:49:03 +0300

    Added TEXT_HTML media type

--- api/src/main/java/org/mosaic/web/net/MediaType.java
+++ api/src/main/java/org/mosaic/web/net/MediaType.java
@@ -14,6 +14,8 @@ public class MediaType implements Cloneable, Serializable
 {
     public static final MediaType PLAIN_TEXT = new MediaType( "text", "plain" );
 
+    public static final MediaType TEXT_HTML = new MediaType( "text", "html" );
+
     private final com.google.common.net.MediaType mediaType;
 
     public MediaType( @Nonnull String mediaType )
