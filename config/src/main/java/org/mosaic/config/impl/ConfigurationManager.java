f2ec13edf4ec9d3aeb4dc18af805bb19caa7689c
arikkfir <arik.kfir@gmail.com>
2013-06-18 03:40:17 +0300

    Fixed attributes injection

--- config/src/main/java/org/mosaic/config/impl/ConfigurationManager.java
+++ config/src/main/java/org/mosaic/config/impl/ConfigurationManager.java
@@ -92,7 +92,7 @@ public class ConfigurationManager
     }
 
     @FileWatcher(root = WatchRoot.ETC,
-                 pattern = "**/*.properties",
+                 pattern = "*.properties",
                  event = { WatchEvent.FILE_ADDED, WatchEvent.FILE_MODIFIED })
     public void onFileModified( @Nonnull Path file, @Nonnull BasicFileAttributes attrs ) throws IOException
     {
