b6624c2afba91aadd029b35d2488baed32709c8f
arikkfir <arik.kfir@gmail.com>
2013-07-01 02:09:24 +0300

    nothing

--- admin/src/main/java/org/mosaic/admin/AdminTest.java
+++ admin/src/main/java/org/mosaic/admin/AdminTest.java
@@ -1,10 +1,13 @@
 package org.mosaic.admin;
 
+import java.io.IOException;
+import javax.annotation.Nonnull;
 import org.mosaic.lifecycle.annotation.Bean;
 import org.mosaic.lifecycle.annotation.Measure;
 import org.mosaic.shell.annotation.Command;
 import org.mosaic.web.handler.annotation.Controller;
 import org.mosaic.web.handler.annotation.WebAppFilter;
+import org.mosaic.web.request.WebResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,17 +19,24 @@ public class AdminTest
 {
     private static final Logger LOG = LoggerFactory.getLogger( AdminTest.class );
 
-    @Command( name = "test" )
+    @Command(name = "test")
     @Measure
     public void testCommand()
     {
         LOG.info( "Test test" );
     }
 
-    @Controller( "/" )
+    @Controller( "/a" )
     @WebAppFilter( "application.name=='test'" )
-    public void testController()
+    public void a( @Nonnull WebResponse response ) throws IOException
     {
-        System.out.println( "hello from test webapp" );
+        response.getCharacterBody().append( "Hello controller 'a'!" );
+    }
+
+    @Controller("/b")
+    @WebAppFilter("application.name=='test'")
+    public void b( @Nonnull WebResponse response ) throws IOException
+    {
+        response.getCharacterBody().append( "Hello controller 'b'!" );
     }
 }
