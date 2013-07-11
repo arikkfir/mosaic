dcc4d5f133f913e2ec756ee4a01d552a355bdeb8
arikkfir <arik.kfir@gmail.com>
2013-07-12 01:05:54 +0300

    [Un]Marshalling annotations must be @MethodEndpointMarker(s)

--- api/src/main/java/org/mosaic/web/marshall/annotation/Unmarshaller.java
+++ api/src/main/java/org/mosaic/web/marshall/annotation/Unmarshaller.java
@@ -4,12 +4,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.mosaic.lifecycle.annotation.MethodEndpointMarker;
 
 /**
  * @author arik
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+@MethodEndpointMarker
 public @interface Unmarshaller
 {
     String consumes();
