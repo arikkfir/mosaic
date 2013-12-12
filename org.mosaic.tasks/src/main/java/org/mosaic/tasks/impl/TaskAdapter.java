181d1aaa0376cf7f4d360242212afd3bf4df8b7a
arik <arik.kfir@gmail.com>
2013-12-12 22:21:51 +0200

    allow @Task methods to receive task properties by simply declaring them as parameters

--- org.mosaic.tasks/src/main/java/org/mosaic/tasks/impl/TaskAdapter.java
+++ org.mosaic.tasks/src/main/java/org/mosaic/tasks/impl/TaskAdapter.java
@@ -1,10 +1,14 @@
 package org.mosaic.tasks.impl;
 
 import java.util.Collections;
+import java.util.Map;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.mosaic.modules.MethodEndpoint;
 import org.mosaic.tasks.Task;
 import org.mosaic.util.collections.MapEx;
+import org.mosaic.util.reflection.MethodParameter;
+import org.mosaic.util.reflection.ParameterResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +28,27 @@ final class TaskAdapter
     TaskAdapter( @Nonnull MethodEndpoint<Task> endpoint )
     {
         this.endpoint = endpoint;
-        this.invoker = this.endpoint.createInvoker();
+        this.invoker = this.endpoint.createInvoker(
+                new ParameterResolver()
+                {
+                    @Nullable
+                    @Override
+                    public Object resolve( @Nonnull MethodParameter parameter,
+                                           @Nonnull MapEx<String, Object> resolveContext )
+                            throws Exception
+                    {
+                        Map properties = resolveContext.require( "properties", Map.class );
+                        if( properties.containsKey( parameter.getName() ) )
+                        {
+                            return properties.get( parameter.getName() );
+                        }
+                        else
+                        {
+                            return SKIP;
+                        }
+                    }
+                }
+        );
     }
 
     @Nonnull
@@ -35,10 +59,9 @@ final class TaskAdapter
 
     void execute( @Nonnull MapEx<String, String> properties )
     {
-        // TODO: pass properties as arguments to @Task method eg '@Task void myTask( int myProperty )'
         try
         {
-            this.invoker.resolve( Collections.<String, Object>emptyMap() ).invoke();
+            this.invoker.resolve( Collections.<String, Object>singletonMap( "properties", properties ) ).invoke();
         }
         catch( Throwable e )
         {
