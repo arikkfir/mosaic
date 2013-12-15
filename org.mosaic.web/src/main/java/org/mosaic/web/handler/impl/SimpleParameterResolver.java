d888126b931568706311fd13fafd4d21cc5f017d
arik <arik.kfir@gmail.com>
2013-12-16 01:25:00 +0200

    add SimpleParameterResolver class

---
+++ org.mosaic.web/src/main/java/org/mosaic/web/handler/impl/SimpleParameterResolver.java
@@ -0,0 +1,37 @@
+package org.mosaic.web.handler.impl;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.mosaic.util.collections.MapEx;
+import org.mosaic.util.reflection.MethodParameter;
+import org.mosaic.util.reflection.ParameterResolver;
+
+/**
+ * @author arik
+ */
+final class SimpleParameterResolver implements ParameterResolver
+{
+    @Nonnull
+    private final String key;
+
+    @Nonnull
+    private final Class<?> type;
+
+    SimpleParameterResolver( @Nonnull String key, @Nonnull Class<?> type )
+    {
+        this.key = key;
+        this.type = type;
+    }
+
+    @Nullable
+    @Override
+    public Object resolve( @Nonnull MethodParameter parameter, @Nonnull MapEx<String, Object> resolveContext )
+            throws Exception
+    {
+        if( parameter.getType().isAssignableFrom( this.type ) )
+        {
+            return resolveContext.require( this.key, this.type );
+        }
+        return SKIP;
+    }
+}
