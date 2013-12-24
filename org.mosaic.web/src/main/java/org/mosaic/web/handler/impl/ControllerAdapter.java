ae01a8dc950a3c7618a12ce0ecc74a7f468e0aa3
arik <arik.kfir@gmail.com>
2013-12-24 02:16:22 +0200

    support authentication in @Controller

--- org.mosaic.web/src/main/java/org/mosaic/web/handler/impl/ControllerAdapter.java
+++ org.mosaic.web/src/main/java/org/mosaic/web/handler/impl/ControllerAdapter.java
@@ -3,26 +3,32 @@ package org.mosaic.web.handler.impl;
 import java.lang.annotation.Annotation;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.mosaic.modules.Adapter;
 import org.mosaic.modules.MethodEndpoint;
 import org.mosaic.modules.Service;
+import org.mosaic.security.AccessDeniedException;
+import org.mosaic.security.Security;
 import org.mosaic.util.collections.MapEx;
 import org.mosaic.util.expression.Expression;
 import org.mosaic.util.expression.ExpressionParser;
+import org.mosaic.web.application.Application;
 import org.mosaic.web.handler.Controller;
 import org.mosaic.web.handler.RequestHandler;
 import org.mosaic.web.handler.spi.HttpMethodMarker;
+import org.mosaic.web.request.HttpStatus;
 import org.mosaic.web.request.WebRequest;
+import org.mosaic.web.security.Authenticator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * @author arik
  */
-@Adapter(RequestHandler.class)
+@Adapter( RequestHandler.class )
 final class ControllerAdapter implements RequestHandler
 {
     private static final Logger LOG = LoggerFactory.getLogger( ControllerAdapter.class );
@@ -47,6 +53,14 @@ final class ControllerAdapter implements RequestHandler
     @Nonnull
     @Service
     private ExpressionParser expressionParser;
+
+    @Nonnull
+    @Service
+    private List<Authenticator> authenticators;
+
+    @Nonnull
+    @Service
+    private Security security;
 
     @Service
     ControllerAdapter( @Nonnull MethodEndpoint<Controller> endpoint )
@@ -111,6 +125,52 @@ final class ControllerAdapter implements RequestHandler
     public Object handle( @Nonnull WebRequest request ) throws Exception
     {
         LOG.debug( "Invoking method endpoint '{}'", this.endpoint );
-        return this.invoker.resolve( Collections.<String, Object>singletonMap( "request", request ) ).invoke();
+        try
+        {
+            return this.invoker.resolve( Collections.<String, Object>singletonMap( "request", request ) ).invoke();
+        }
+        catch( AccessDeniedException e )
+        {
+            // if user already authenticated, no use in re-authenticating - just deny access as well
+            if( this.security.getSubject().isAuthenticated() )
+            {
+                request.getResponse().setStatus( HttpStatus.FORBIDDEN );
+                return null;
+            }
+
+            // get auth method from exception; if none, see if request matches a security constraint
+            String authMethod = e.getAuthenticationMethod();
+            if( authMethod == null )
+            {
+                Application.ApplicationSecurity.SecurityConstraint securityConstraint = request.getSecurityConstraint();
+                if( securityConstraint != null )
+                {
+                    authMethod = securityConstraint.getAuthenticationMethod();
+                }
+            }
+
+            // if not found auth method (exception / security-constraint), just deny access
+            if( authMethod == null )
+            {
+                // if no security constraint, we don't know HOW users should authenticate, so just deny access
+                request.getResponse().setStatus( HttpStatus.FORBIDDEN );
+                return null;
+            }
+
+            // we have auth method - find appropriate authenticator and use it to send a challange to the client
+            for( Authenticator authenticator : this.authenticators )
+            {
+                if( authenticator.getAuthenticationMethods().contains( authMethod ) )
+                {
+                    authenticator.challange( request );
+                    return null;
+                }
+            }
+
+            // if we got here - no authenticator matched; fail request and log
+            request.getResponse().setStatus( HttpStatus.INTERNAL_SERVER_ERROR );
+            request.dumpToWarnLog( LOG, "Could not find '{}' authenticator", authMethod, e );
+            return null;
+        }
     }
 }
