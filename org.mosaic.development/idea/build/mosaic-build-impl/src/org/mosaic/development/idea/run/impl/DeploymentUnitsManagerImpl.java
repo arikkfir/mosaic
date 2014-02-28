5773fe0eb725b73790113cd37999b1ab87ad2561
arik <arik.kfir@gmail.com>
2014-02-28 23:43:15 +0200

    Sort modules in deployment tab of Mosaic run configurations

--- org.mosaic.development/idea/build/mosaic-build-impl/src/org/mosaic/development/idea/run/impl/DeploymentUnitsManagerImpl.java
+++ org.mosaic.development/idea/build/mosaic-build-impl/src/org/mosaic/development/idea/run/impl/DeploymentUnitsManagerImpl.java
@@ -7,6 +7,8 @@ import com.intellij.openapi.roots.impl.libraries.ProjectLibraryTable;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.LibraryTable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import org.jetbrains.annotations.NotNull;
 import org.mosaic.development.idea.facet.OsgiBundleFacet;
@@ -46,6 +48,34 @@ public class DeploymentUnitsManagerImpl extends DeploymentUnitsManager
             deploymentUnits.add( DeploymentUnit.projectLibrary( this.myProject, library ) );
         }
 
+        Collections.sort( deploymentUnits, new Comparator<DeploymentUnit>()
+        {
+            @Override
+            public int compare( DeploymentUnit o1, DeploymentUnit o2 )
+            {
+                if( o1 == null && o2 != null )
+                {
+                    return -1;
+                }
+                else if( o1 != null && o2 == null )
+                {
+                    return 1;
+                }
+                else if( o1 == null )
+                {
+                    return 0;
+                }
+
+                if( o1.getType() != o2.getType() )
+                {
+                    return o1.getType().compareTo( o2.getType() );
+                }
+                else
+                {
+                    return o1.getName().compareTo( o2.getName() );
+                }
+            }
+        } );
         return deploymentUnits;
     }
 }
