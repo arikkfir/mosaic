5cd68b19f587b89da072a38fb7c50f481ffb806e
arik <arik.kfir@gmail.com>
2014-03-11 03:03:53 +0200

    More work on demo

--- org.mosaic.demo/org.mosaic.demo.accounting/src/main/resources/WEB-INF/index.js
+++ org.mosaic.demo/org.mosaic.demo.accounting/src/main/resources/WEB-INF/index.js
@@ -1,5 +1,33 @@
 "use strict";
 
+/**
+ * Routes the UI to the About Us page.
+ */
+function home() {
+    alert( "Home!" );
+}
+function about() {
+    alert( "Learn more!?" );
+}
+
+// setup application routes
+Path.map( "#" ).to( home );
+Path.map( "#/about" ).to( about );
+Path.root( "#" );
+
+// handle unknown routes
+Path.rescue( function() {
+    alert( "404: Route Not Found" );
+} );
+
+// start PathJS
+$( document ).ready( function() {
+    Path.listen();
+} );
+
+/**
+ * Performs logout
+ */
 function logout() {
     // TODO arik: implement
 }
