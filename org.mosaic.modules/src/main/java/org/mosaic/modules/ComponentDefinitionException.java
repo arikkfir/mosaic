b9aa4671ff3f1461ae3af3f59fe01ebb0eedf303
arik <arik.kfir@gmail.com>
2014-03-20 23:07:27 +0200

    Add constructors to ComponentDefinitionException

--- org.mosaic.modules/src/main/java/org/mosaic/modules/ComponentDefinitionException.java
+++ org.mosaic.modules/src/main/java/org/mosaic/modules/ComponentDefinitionException.java
@@ -15,6 +15,11 @@ public class ComponentDefinitionException extends ComponentException
         super( "bad component definition for type '" + type + "' in module " + module + ": " + message, type, module );
     }
 
+    public ComponentDefinitionException( @Nonnull String message, @Nonnull TypeDescriptor typeDescriptor )
+    {
+        super( "bad component definition for type '" + typeDescriptor.getType() + "' in module " + typeDescriptor.getModule() + ": " + message, typeDescriptor.getType(), typeDescriptor.getModule() );
+    }
+
     public ComponentDefinitionException( @Nonnull String message,
                                          @Nonnull Throwable cause,
                                          @Nonnull Type type,
@@ -23,10 +28,22 @@ public class ComponentDefinitionException extends ComponentException
         super( "bad component definition for type '" + type + "' in module " + module + ": " + message, cause, type, module );
     }
 
+    public ComponentDefinitionException( @Nonnull String message,
+                                         @Nonnull Throwable cause,
+                                         @Nonnull TypeDescriptor typeDescriptor )
+    {
+        super( "bad component definition for type '" + typeDescriptor.getType() + "' in module " + typeDescriptor.getModule() + ": " + message, cause, typeDescriptor.getType(), typeDescriptor.getModule() );
+    }
+
     public ComponentDefinitionException( @Nonnull Throwable cause,
                                          @Nonnull Type type,
                                          @Nonnull Module module )
     {
         super( "bad component definition for type '" + type + "' in module " + module, cause, type, module );
+    }
+
+    public ComponentDefinitionException( @Nonnull Throwable cause, @Nonnull TypeDescriptor typeDescriptor )
+    {
+        super( "bad component definition for type '" + typeDescriptor.getType() + "' in module " + typeDescriptor.getModule(), cause, typeDescriptor.getType(), typeDescriptor.getModule() );
     }
 }
